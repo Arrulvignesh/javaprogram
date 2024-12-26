daf50a52febb70da53fde7fbd1afe0ac1ca27647
Arrulvignesh <tarrulvignesh@gmail.com>
2024-12-26 11:37:53 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,15 @@
+package multiplicatontable;
+
+public class multtable {
+	    public static void main(String[] args) {
+	       
+	        for (int i = 1; i <= 10; i++) {
+	            System.out.println(i);
+	      for (int j = 1; j <= 10; j++) {
+	              System.out.printf("%d x %d = %d\n", i, j, i * j);
+	                 }
+	            System.out.println();
+	        }
+	    }
+	
+}
